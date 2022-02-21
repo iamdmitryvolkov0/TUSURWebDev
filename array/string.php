8c9be8ddea58b6263616bd6cd12f877276dd6640
iamdmitryvolkov0 <iamdmitryvolkov@gmail.com>
2022-02-21 18:10:30 +0700

    add file to string processing

---
+++ array/string.php
@@ -0,0 +1,22 @@
+<?php
+
+$data_txt = file_get_contents('text.txt');
+$data_strings = preg_split("/\n/", $data_txt);
+$char_index = 0;
+$complete = '';
+
+foreach ($data_strings as $s) {
+    $data_chars = preg_split('//', $s);
+    $replace = '';
+    foreach ($data_chars as $char)  //посимвольно читаем
+    {
+        $replace .= ' |'.$char_index.'| '.$char;
+        $char_index++;
+    }
+    if (is_numeric($s[0]))  //если начинается с цифры, добавляем скобки
+    {
+        $replace = "($replace)";
+    }
+    $complete .= "$replace\n";
+}
+var_dump($complete);
